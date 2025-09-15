ac17d2a57acc33bd32b9c1155d9fac9fa6f15d8b
Cooljoe464 <Cooljoe464@gmail.com>
2025-09-15 13:41:20 +0100

    Update aircraft-table.blade.php

--- resources/views/livewire/aircraft-table.blade.php
+++ resources/views/livewire/aircraft-table.blade.php
@@ -3,7 +3,7 @@
         <input type="text" wire:model.live.debounce.300ms="search" placeholder="Search aircrafts..." class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full sm:w-1/3">
         <div class="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2">
             <button wire:click="export" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full sm:w-auto">Export to CSV</button>
-            <a href="{{ route('aircrafts.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full sm:w-auto text-center">Create Aircraft</a>
+            <a href="{{ route('aircraft.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full sm:w-auto text-center">Create Aircraft</a>
         </div>
     </div>
 
@@ -24,8 +24,8 @@
                         <tr>
                             <td class="px-6 py-4 whitespace-nowrap">{{ $aircraft->name }}</td>
                             <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                <a href="{{ route('aircrafts.edit', $aircraft) }}" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                <form action="{{ route('aircrafts.destroy', $aircraft) }}" method="POST" class="inline-block">
+                                <a href="{{ route('aircraft.edit', $aircraft) }}" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                <form action="{{ route('aircraft.destroy', $aircraft) }}" method="POST" class="inline-block">
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit" class="text-red-600 hover:text-red-900 ml-4">Delete</button>
@@ -46,8 +46,8 @@
                     <div class="flex justify-between items-center">
                         <span class="font-bold text-lg">{{ $aircraft->name }}</span>
                         <div class="text-right">
-                            <a href="{{ route('aircrafts.edit', $aircraft) }}" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                            <form action="{{ route('aircrafts.destroy', $aircraft) }}" method="POST" class="inline-block">
+                            <a href="{{ route('aircraft.edit', $aircraft) }}" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                            <form action="{{ route('aircraft.destroy', $aircraft) }}" method="POST" class="inline-block">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="text-red-600 hover:text-red-900 ml-4">Delete</button>
